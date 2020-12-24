639c68a7a1965a30f2fe004683b2e85544b09c64
Francisco Pereira <fcpereira12@hotmail.com>
2020-12-24 11:27:57 +0000

    Add client

---
+++ client copy/app/components/Event.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+export default class Event extends Component {
+  render() {
+    return (
+
+        <Content>
+            <Card transparent>
+
+            <CardItem>
+              <Body>
+                <Image source={require('../assets/WalkingTour.jpg')} style={{height: 200, width: 200, flex: 1}}/>
+                <Text>
+                  //Your text here
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent textStyle={{color: '#87838B'}}>
+                  <Icon name="logo-github" />
+                  <Text>1,926 stars</Text>
+                </Button>
+              </Left>
+            </CardItem>
+            </Card>
+        </Content>
+
+    );
+  }
+}
