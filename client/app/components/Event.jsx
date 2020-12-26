7a5802abb0363d363994bb54fb3d1c050942a909
Rita <ritanorinho98@hotmail.com>
2020-12-26 17:55:39 +0000

    Change date format and add event card info

--- client/app/components/Event.jsx
+++ client/app/components/Event.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, H1,H2} from 'native-base';
 import { withNavigation } from 'react-navigation' 
 import  EventService  from '../services/EventService';
+import Moment from 'moment';
 class Event extends Component {
+
+  static navigationOptions = {
+    title: "Event",
+  };
   constructor(props){
     super(props);
     this.EventService = new EventService();
@@ -11,9 +17,11 @@ class Event extends Component {
         title: "",
         date: "",
         location: "",
-        category: "",
-        eventId: "5fe4b4d4c6dd2a9cb83b5bee"
+        eventId: "5fe4b4d4c6dd2a9cb83b5bee",
+        description:"",
+        price:"",
       },
+      category: "",
     }
   }
   async componentDidMount() {
@@ -24,6 +32,7 @@ class Event extends Component {
         const { event } = data;
         this.setState({
           event: event,
+          category: data.categoryName,
           
         });
         console.log(data);
@@ -32,11 +41,9 @@ class Event extends Component {
   }
 
   
-  static navigationOptions = {
-    title: "Event",
-  };
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, H1,H2} from 'native-base';
   render() {
+    Moment.locale('en');
+    var dt = this.state.event.date;
     return (
         <Content>
             <Card transparent>
@@ -44,10 +51,10 @@ import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Ic
                 <Body>
                   <Image source={require('../assets/WalkingTour.jpg')} style={{height: 200, width: 200, flex: 1}}/>
                   <H1 style={{ fontWeight: 'bold' }}>
-                    Porto Walking Tour
+                    {this.state.event.title}
                   </H1>
                   <Text>
-                    March 11th 2021 {"\n"}14h00
+                  {Moment(dt).format('dd MM yyyy HH:mm')}
                   </Text>
                   </Body>
               </CardItem>
@@ -55,20 +62,17 @@ import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Ic
                   <Text>Category:</Text>
                   <Button iconLeft transparent>
                     <Icon name='walk' />
-                    <Text style= {{paddingLeft: 0}}>Tour</Text>
+                    <Text style= {{paddingLeft: 0}}>{this.state.category}</Text>
                   </Button>
               </CardItem>
               <CardItem> 
                   <Text>
-                  Walking distance around 3km, several downhill steps {"\n"}{"\n"}
-                  Wander through the cobbled streets, breath-taking viewpoints and charming squares of downtown Porto with experienced local guides
-                  {"\n"}Discover our locals' favourite places, best Porto's food and learn about its rich history: a bit of history of Portugal in São Bento railway station and why are there so many churches in Porto?
-                  {"\n"}{"\n"}We visit São Bento railway station, Santa Catarina, Batalha square, Santa Clara church, top of the Bridge Luis I, Cathedral (Sé), Barredo neighborhood, Ribeira quarter.
+                  {this.state.event.description}
                   </Text>
               </CardItem>
               <CardItem> 
                 <H2>
-                  {"\n"}Price: 15€ p/person
+                  {"\n"}Price: {this.state.event.price}
                 </H2>
               </CardItem> 
               <CardItem> 
--- client/app/components/EventCard.jsx
+++ client/app/components/EventCard.jsx
@@ -2,21 +2,56 @@ import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, H3, H1} from 'native-base';
 import { withNavigation } from 'react-navigation'
+import  EventService  from '../services/EventService';
+import Moment from 'moment';
 class EventCard extends Component {
 
   static navigationOptions = {
     title: "EventCard",
   };
+  constructor(props){
+    super(props);
+    this.EventService = new EventService();
+    this.state = {
+    event: {
+        title: "",
+        date: "",
+        location: "",
+        eventId: "5fe4b4d4c6dd2a9cb83b5bee",
+        description:"",
+        price:"",
+        photo:"",
+      },
+      category: "",
+    }
+  }
+  async componentDidMount() {
+     const {eventId} = this.state.event;
+    await this.EventService.getEvent({eventId}, async (res) => {
+      if (res.status == 200) {
+        const { data } = res;
+        const { event } = data;
+        this.setState({
+          event: event,
+          category: data.categoryName,
+          
+        });
+        console.log(data);
+      }
+    });
+  }
   render() {
+    Moment.locale('en');
+    var dt = this.state.event.date;
     return (
           <Card style={{flex: 0}}>
             <CardItem>
               <Left>
-                <Image source={require('../assets/WalkingTour.jpg')} style={{height: 200, width: 200, flex: 1}}/>
+                <Image source={require("../assets/WalkingTour.jpg" )} style={{height: 200, width: 200, flex: 1}}/>
                 <Body>
-                  <H1>Porto Tour</H1>
-                  <Text note>March 11th 2021 16h00</Text>
-                  <H3>15€</H3>
+                  <H1>{this.state.event.title}</H1>
+                  <Text note>{Moment(dt).format('dd MM yyyy HH:mm')}</Text>
+                  <H3>{this.state.event.price}</H3>
                 </Body>
               </Left>
             </CardItem>
